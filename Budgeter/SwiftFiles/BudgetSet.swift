e06f52fe95d3e12ff931032783021074c20c413a
Vaibhav <sahaivaibhav4@gmail.com>
2019-12-28 11:56:11 +0400

    Working On Budget Set Page

---
+++ Budgeter/SwiftFiles/BudgetSet.swift
@@ -0,0 +1,32 @@
+//
+//  BudgetSet.swift
+//  Budgeter
+//
+//  Created by Vaibhav Sahai on 28/12/2019.
+//  Copyright © 2019 Vaibhav Sahai. All rights reserved.
+//
+
+import UIKit
+
+class BudgetSet: UIViewController {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var budgetText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
