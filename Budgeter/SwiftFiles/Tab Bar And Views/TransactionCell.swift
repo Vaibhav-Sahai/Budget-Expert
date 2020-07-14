f98ff563e73a71a78886e6afa97de3fe55a6d8e1
Vaibhav <sahaivaibhav4@gmail.com>
2020-07-14 22:05:14 +0400

    Made table view, Pre taking in data

---
+++ Budgeter/SwiftFiles/Tab Bar And Views/TransactionCell.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionCell.swift
+//  Budgeter
+//
+//  Created by Vaibhav Sahai on 14/07/2020.
+//  Copyright © 2020 Vaibhav Sahai. All rights reserved.
+//
+
+import UIKit
+
+class TransactionCell: UITableViewCell {
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var currencySymbol: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+    
+}
--- Budgeter/SwiftFiles/Tab Bar And Views/TransactionLog.swift
+++ Budgeter/SwiftFiles/Tab Bar And Views/TransactionLog.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class TransactionLog: UIViewController {
-
+class TransactionLog: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let data = ["1","2","3","4"]
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    //MARK:- Table View Setup
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TransactionCell
+        
+        cell.itemName.text = data[indexPath.row]
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
